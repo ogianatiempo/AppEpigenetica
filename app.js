a03354f3a7019448fa8e1706894384cdcedc6f99
Octavio Gianatiempo <ogianatiempo@gmail.com>
2017-07-21 12:04:50 -0300

    add packager and storage dependencies

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var electron = require('electron')
 
 electron.app.on('ready', function () {
- var mainWindow = new electron.BrowserWindow({width: 800, height: 600})
+ var mainWindow = new electron.BrowserWindow({width: 800, height: 600,
+    titleBarStyle: 'hidden',
+    icon: __dirname + '/assets/icons/png/64x64.png'
+ })
  mainWindow.loadURL('file://' + __dirname + '/index.html')
 })
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+// Requirements
 var remote = require('electron').remote
 var fs = require('fs')
+const storage = require('electron-storage');
 window.$ = window.jQuery = require('jquery')
 require('bootstrap')
 
-function save () {
+// Save functions
+function save_pdf () {
  remote.getCurrentWindow().webContents.printToPDF({
    portrait: false
  }, function (err, data) {
@@ -14,10 +17,18 @@ function save () {
  })
 }
 
+
+// Event listeners
 window.addEventListener('keydown', function (e) {
-  if (e.keyCode == 80) save()
-})
+  if (e.keyCode == 80) {
+  };
+});
 
 $("#btn-print").click(function(){
-  save()
+  save_pdf()
+})
+
+$("#btn-save").click(function(){
+  alert($("#nombre").val());
+  alert($("#texto").val())
 })
